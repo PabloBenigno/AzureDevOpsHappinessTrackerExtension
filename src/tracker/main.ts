07e8a5ee16c26867a9e10b66a16d16dcc7d68a84
Pablo Pérez Camarena <pablo.perez@soderbergpartners.se>
2020-04-17 13:25:44 +0200

    added simple user list

--- src/tracker/main.ts
+++ src/tracker/main.ts
@@ -1,4 +1,16 @@
+// import RestClient = require("TFS/Core/RestClient");
+
 function getUserName () {
     "use strict";
     return VSS.getWebContext().user.name;
 };
+
+// function getTeamMembers () {
+    // var ctx = VSS.getWebContext();
+
+    // // Get an instance of the client
+    //  var client = RestClient.getClient();
+    // var members = client.getTeamMembersWithExtendedProperties(ctx.project.id, ctx.team.id, 10, 0);
+    // console.log(members);
+//     return members;
+// }
